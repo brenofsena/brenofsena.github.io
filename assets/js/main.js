9df81321ed32dbb94ab4ce1b6f92e342e068b8d1
Breno Sena <breno.fsena@gmail.com>
2017-02-24 10:44:26 -0300

    removing jquery

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,21 @@
-$(document).ready(function(){
-	$('.open-nav').click(function(){
-		$('.header').addClass('menu-active');
+function addClass(el, className){
+	el.classList.add(className);
+};
+
+function removeClass(el, className){
+	el.classList.remove(className);
+};
+
+window.onload = function(){
+	var open = document.getElementById('open-nav'),
+		close = document.getElementById('close-nav'),
+		header = document.getElementById('header');
+
+	open.addEventListener('click', function(){
+		addClass(header, 'menu-active');
 	});
-	$('.close-nav').click(function(){
-		$('.header').removeClass('menu-active');
+
+	close.addEventListener('click', function(){
+		removeClass(header, 'menu-active');
 	});
-});
+};
