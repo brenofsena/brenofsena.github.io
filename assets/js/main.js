f7a5911619bc6f2c650d8673731a9703ca0504d2
breno <breno.sena@corebiz.com.br>
2019-07-24 01:49:57 -0300

    Padronização de classes css com RSCSS

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,15 @@
-let $ = document.querySelector.bind(document);
+const $ = document.querySelector.bind(document);
 
 $('.menu-open').onclick = function () {
-  document.documentElement.classList.add('menu-active');
+  document.documentElement.classList.add('-menu-active');
 };
 
 $('.menu-close').onclick = function () {
-  document.documentElement.classList.remove('menu-active');
+  document.documentElement.classList.remove('-menu-active');
 };
 
 document.documentElement.onclick = function (event) {
   if (event.target == document.documentElement) {
-    document.documentElement.classList.remove('menu-active');
+    document.documentElement.classList.remove('-menu-active');
   }
 };
